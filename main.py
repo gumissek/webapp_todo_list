87e61f0fa0cfc81e69201199610de5c1cd38341c
PiotrekB <bilinski.piotr89@gmail.com>
2025-02-25 21:44:53 +0100

    delete tasks

--- main.py
+++ main.py
@@ -7,73 +7,75 @@ from sqlalchemy import Integer, String, Boolean, DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from forms import AddTaskForm
 
-#TODO
-#todo strona we flasku ktora pokazuje zadania tymczasowe i stale odziellnie
-#todo zadanie tymczasowe- obiekt: status wykonania, tekst,czas wykonania
-#todo formularz do tworzenia zadan tymczasowych:ma pola test i data, domyslnie ma status wykonania na false(jako radio button)
-# todo tworzenie listy z zadan tymczaowych po wypelnieniu formlarza
-#todo wyswietlanie tej listy jako zadan tymczasowych
-#todo jak mamy ta liste i kliknemy guzik to przjedzie po tych zadaniach tymczasowych i zapisze je w bazie danych jako zadania na stale
-#todo wyciagamy zdania z bazy danych i wyswietlamy jako osobna lista ponziej zadan tymczasowych -> potem
+# done strona we flasku ktora pokazuje zadania tymczasowe i stale odziellnie
+# done zadanie tymczasowe- obiekt: status wykonania, tekst,czas wykonania
+# done formularz do tworzenia zadan tymczasowych:ma pola test i data, domyslnie ma status wykonania na false(jako radio button)
+# done tworzenie listy z zadan tymczaowych po wypelnieniu formlarza
+# done wyswietlanie tej listy jako zadan tymczasowych
+# done jak mamy ta liste i kliknemy guzik to przjedzie po tych zadaniach tymczasowych ,zapisze je w bazie danych i usunie z temporary
+# done wyciagamy zdania z bazy danych i wyswietlamy jako osobna lista ponziej zadan tymczasowych -> potem do oznaczenia jako wykonane i usuwanie
+# todo sortowanie tabeli po statusie wykonania
+# todo przycisk zmiany statusu wykonania zmienia sie w zaleznosci od statusu zdania
+
+# todo strona glowna ktora zawiera zadania
+# todo strona glowna: zawiera forma ktory ma tekst i date do kiedy
 
 
-#todo strona glowna ktora zawiera zadania
-#todo strona glowna: zawiera forma ktory ma tekst i date do kiedy
+# todo layout
+# todo zmienic debugowanie jesli dziala jak nalezy  strona
+
+FLASK_KEY = os.getenv('FLASK_KEY', '123312')
+DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///todo_task.db')
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+# CONFIGS
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SECRET_KEY'] = FLASK_KEY
 
 
-#todo layout
-#todo zmienic debugowanie jak dziala strona
-
-FLASK_KEY=os.getenv('FLASK_KEY','123312')
-DATABASE_URI=os.getenv('DATABASE_URI','sqlite:///todo_task.db')
-
-
-app=Flask(__name__)
-bootstrap=Bootstrap5(app)
-
-#CONFIGS
-app.config['SQLALCHEMY_DATABASE_URI']= DATABASE_URI
-app.config['SECRET_KEY']=FLASK_KEY
-
-
-#DATABASE
+# DATABASE
 class Base(DeclarativeBase):
     pass
 
-db=SQLAlchemy(model_class=Base)
+
+db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-#TABLES
+
+# TABLES
 class Task(db.Model):
-    __tablename__= 'tasks'
-    id : Mapped[int] = mapped_column(Integer,primary_key=True)
-    text : Mapped[str] = mapped_column(String(250),nullable=False)
-    status : Mapped[bool] = mapped_column(Boolean,nullable=False)
-    date_start : Mapped[str] = mapped_column(String(250),nullable=False)
-    date_end : Mapped[str] = mapped_column(String(250),nullable=False)
+    __tablename__ = 'tasks'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    text: Mapped[str] = mapped_column(String(250), nullable=False)
+    status: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    date_start: Mapped[str] = mapped_column(String(250), nullable=False)
+    date_end: Mapped[str] = mapped_column(String(250), nullable=False)
+
 
 with app.app_context():
     db.create_all()
 
-
 temporary_tasks = []
 
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def homepage():
-    add_task_form=AddTaskForm()
-    saved_tasks=db.session.execute(db.select(Task)).scalars().all()
+    add_task_form = AddTaskForm()
+    saved_tasks = db.session.execute(db.select(Task)).scalars().all()
     if add_task_form.validate_on_submit():
-        task_text=request.form['text']
+        task_text = request.form['text']
         task_date_end = request.form['date_end']
-        new_task=Task(text=task_text,status=False,date_start=datetime.datetime.now().strftime('%d-%m-%Y'),date_end=task_date_end)
+        new_task = Task(text=task_text, status=False, date_start=datetime.datetime.now().strftime('%d-%m-%Y'),
+                        date_end=task_date_end)
         temporary_tasks.append(new_task)
         return redirect(url_for('homepage'))
 
-    return render_template('index.html', addtaskform=add_task_form,temporary_tasks=temporary_tasks,saved=saved_tasks)
+    return render_template('index.html', addtaskform=add_task_form, temporary_tasks=temporary_tasks, saved=saved_tasks)
 
 
-@app.route('/save',methods=['GET','POST'])
+@app.route('/save', methods=['GET', 'POST'])
 def save_temporary():
     for task in temporary_tasks:
         db.session.add(task)
@@ -83,16 +85,24 @@ def save_temporary():
     return redirect(url_for('homepage'))
 
 
-@app.route('/mark',methods=['POST','GET'])
-def mark_done():
-    task_to_mark=db.session.execute(db.select(Task).where(Task.id==request.args.get('task_id'))).scalar()
+@app.route('/mark', methods=['POST', 'GET'])
+def mark_as_done():
+    task_to_mark = db.session.execute(db.select(Task).where(Task.id == request.args.get('task_id'))).scalar()
     if task_to_mark.status:
-        task_to_mark.status=False
+        task_to_mark.status = False
     else:
-        task_to_mark.status=True
+        task_to_mark.status = True
     db.session.commit()
     return redirect(url_for('homepage'))
 
-if __name__=='__main__':
-    app.run(debug=True,port=5001)
 
+@app.route('/remove', methods=['POST', 'GET'])
+def delete_task():
+    task_to_remove = db.session.execute(db.select(Task).where(Task.id == request.args.get('delete_id'))).scalar()
+    db.session.delete(task_to_remove)
+    db.session.commit()
+    return redirect(url_for('homepage'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
